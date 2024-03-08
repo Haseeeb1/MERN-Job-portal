f0af6aeae164d097b87554d40f403d0581e361b4
Ibadullah5 <ibadullahriaz5@gmail.com>
2024-03-08 14:27:19 +0500

    added env in frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,7 +16,7 @@ library.add(fab);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <Provider store={store}>
       <BrowserRouter>
         <App />
